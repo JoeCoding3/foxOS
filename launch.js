0f76e6155ad9cd3cf02a2bf9c46c84a04d837462
JoeCoding3 <tiredredstoner@gmail.com>
2024-09-06 21:57:20 -0700

    modified:   index.html 	new file:   launch.js 	new file:   style.css

---
+++ launch.js
@@ -0,0 +1,22 @@
+function launch (path, aboutBlank, init) {
+    let optsStr = `popup,left=0,top=0,width=${screen.width},height=${screen.height}`
+    let jsStr = `<script>onunload = function () { close() }; setTimeout(() => dispatchEvent(new Event("load")), 100)<\/script>`
+    let frameStr = `<iframe style="width: 100%; height: 100%; position: absolute; left: 0px; top: 0px;" frameborder="0" src="${path}"><\/iframe>${jsStr}`
+    
+    let url = path.startsWith("https://") || path.startsWith("./")
+    if (url) {
+        if (aboutBlank) {
+            let win = open(undefined, "_blank", optsStr)
+            if (win == null) {
+                document.body.innerText = "Please allow popups and reload."
+                stop()
+            } else {
+                win.document.write(frameStr)
+                if (init) {
+                    document.body.innerText = ""
+                    close()
+                }
+            }
+        } else open(path, "_blank", optsStr)
+    } else open(undefined, "_blank", optsStr).document.write(atob(path) + jsStr)
+}
