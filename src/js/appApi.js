11d224bb205dc031ada77fd56af403f4faf615fd
JoeCoding3 <tiredredstoner@gmail.com>
2024-09-06 23:47:16 -0700

    new file:   src/app/icon/blank.png 	new file:   src/app/icon/camera.png 	new file:   src/app/icon/chrome.png 	new file:   src/app/icon/chrome2.png 	new file:   src/app/icon/clever.png 	new file:   src/app/icon/desmos.png 	new file:   src/app/icon/dino.png 	new file:   src/app/icon/drive.png 	new file:   src/app/icon/filesys.png 	new file:   src/app/icon/gmail.png 	new file:   src/app/icon/grades.png 	new file:   src/app/icon/hexedit.png 	new file:   src/app/icon/imgedit.png 	new file:   src/app/icon/logs.png 	new file:   src/app/icon/mc.png 	new file:   src/app/icon/schoology.png 	new file:   src/app/icon/screensaver.png 	new file:   src/app/icon/terminal.png 	new file:   src/app/icon/textedit.png 	new file:   src/app/src/blank.html 	new file:   src/app/src/camera.html 	new file:   src/app/src/dino.html 	new file:   src/app/src/filesys.html 	new file:   src/app/src/hexedit.html 	new file:   src/app/src/imgedit.html 	new file:   src/app/src/mc.html 	new file:   src/app/src/screensaver.html 	new file:   src/app/src/terminal.html 	new file:   src/app/src/textedit.html 	modified:   src/css/style.css 	modified:   src/js/appApi.js

--- src/js/appApi.js
+++ src/js/appApi.js
@@ -1,13 +1,47 @@
-function registerApp (name, ab = true, hidden = false, security = 0) {
+function registerApp (name = "", posX = 0, posY = 0, ab = true, hidden = false, security = 0, url = "") {
     let img = document.createElement("img")
     img.src = `./app/icon/${name}.png`
     img.onclick = function () {
-        launch(`./app/src/${this.name}.html`, ab)
-    }.bind({name, ab})
+        if (url != "") launch(url, ab)
+        else launch(`./app/src/${this.name}.html`, ab)
+    }.bind({name, ab, url})
     img.className = `${hidden ? "hide " : ""}sec${security}`
+    if (posX < 0) img.style.left = `${screen.width + (posX * 74) + 10}px`
+    else img.style.left = `${(posX * 74) + 10}px`
+    if (posY < 0) img.style.top = `${screen.height + (posY * 74) + 10}px`
+    else img.style.top = `${(posY * 74) + 10}px`
     document.body.append(img)
 }
 
 function registerAllApps () {
-    registerApp("clock")
+    registerApp("chrome",       0,  0, false, false, 3, "https://www.google.com/?safe=active&ssui=on")
+    registerApp("chrome2",      1,  0, true,  false, 1, "https://www.google.com/?safe=active&ssui=on&igu=1")
+    
+    registerApp("gmail",        0,  1, false, false, 3, "https://mail.google.com/mail/u/0/#inbox")
+    registerApp("drive",        1,  1, false, false, 3, "https://drive.google.com/drive/my-drive")
+    
+    registerApp("blank",        0,  2, true,  false, 0, "")
+    registerApp("terminal",     1,  2, true,  false, 0, "")
+
+    registerApp("textedit",     0,  3, true,  false, 0, "")
+    registerApp("hexedit",      1,  3, true,  false, 0, "")
+    
+    registerApp("camera",       0,  4, true,  false, 0, "")
+    registerApp("imgedit",      1,  4, true,  false, 0, "")
+    
+    registerApp("mc",           0,  5, true,  false, 0, "")
+    registerApp("dino",         1,  5, true,  false, 0, "")
+
+    
+    registerApp("clock",       -1,  0, true,  false, 0, "")
+    registerApp("filesys",     -1,  1, true,  false, 0, "")
+    registerApp("screensaver", -1,  2, true,  false, 0, "")
+
+    
+    registerApp("clever",       0, -1, false, false, 3, "https://clever.com/in/northshore/student/portal")
+    registerApp("grades",       1, -1, false, false, 3, "https://wa-nor-psv.edupoint.com/PXP2_Login_Student.aspx?regenerateSessionId=True")
+    registerApp("schoology",    2, -1, false, false, 3, "https://nsd.schoology.com/home")
+    
+
+    registerApp("desmos",      -1, -1, false, false, 3, "https://student.desmos.com/")
 }
