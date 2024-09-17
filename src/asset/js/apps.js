897c9aec21f8e28dfd9d30e148d5d7387c9d85a7
JoeCoding3 <tiredredstoner@gmail.com>
2024-09-16 19:21:34 -0700

    deleted:    dev/app_additions.yaml 	new file:   src/app/icon/britannica.png 	new file:   src/app/icon/noodletools.png 	new file:   src/app/icon/welnet.png 	new file:   src/app/icon/worldbook.png 	modified:   src/asset/js/apps.js

--- src/asset/js/apps.js
+++ src/asset/js/apps.js
@@ -25,8 +25,14 @@ function registerAllApps () {
 
     registerApp("desmos",       0,  3, false, "https://student.desmos.com/")
     registerApp("textbook",     1,  3, false, "https://reader.savvasrealize.com/book/8S86CIR2XC/view/single/page/1")
+    
+    registerApp("welnet",       0,  4, false, "https://clever.com/oauth/authorize?channel=clever-portal&client_id=c968423122e865b5cdd3&confirmed=true&district_id=5b7b3a1da47b2e0001fba6ac&redirect_uri=https%3A%2F%2Fwww.focusedfitness.net%2FWELNET%2Fssoclevergateway.php&response_type=code")
+    registerApp("noodletools",  1,  4, false, "https://my.noodletools.com/logon/signin?domain=apps.nsd.org")
+
+    registerApp("britannica",   0,  5, false, "https://go.openathens.net/redirector/kcls.org?url=https%3A%2F%2Flibrary.eb.com")
+    registerApp("worldbook",    1,  5, false, "https://worldbookonline.com/advanced/home")
 
     
-    registerApp("mc",           0,  4, true,  "")
-    registerApp("dino",         1,  4, true,  "")
+    registerApp("mc",           0,  6, true,  "")
+    registerApp("dino",         1,  6, true,  "")
 }
